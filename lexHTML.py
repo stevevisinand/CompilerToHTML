3f4b9ad9562cfc5e6fce4315f92fe080d4e8e4c2
lukas Bitter <lukas.bitter@he-arc.ch>
2015-12-03 10:56:10 +0100

    imporve lex: add string and comments management

--- lexHTML.py
+++ lexHTML.py
@@ -1,7 +1,7 @@
 import ply.lex as lex
 
 reserved_words = (
-	'element',
+    'element',
     'title',
     'color',
     'paragraph',
@@ -10,61 +10,63 @@ reserved_words = (
     'for',
     'content',
     'page',
-	'print'
+    'print'
 )
 
 tokens = (
-	'IDENTIFIER',
+    'IDENTIFIER',
     'STRING',
     'NUMBER',
     'COMMENT',
-	'ADD_OP'
+    'ADD_OP'
 ) + tuple(map(lambda s:s.upper(),reserved_words))
 
 literals = '(),:;={}[]"/'
 
 def t_IDENTIFIER(t):
-	r'[A-Za-z_]\w*'
-	if t.value in reserved_words:
-		t.type = t.value.upper()
-	return t
+    r'[A-Za-z_]\w*'
+    if t.value in reserved_words:
+        t.type = t.value.upper()
+    return t
 
 def t_STRING(t):
-	r'"[A-Za-z _]*"'
-	return t
+    r'"[A-Za-z _]*\"'
+    t.value = t.value[1:-1]
+    return t
 
 def t_NUMBER(t):
-	r'\d+(\.\d+)?'
-	try:
-		t.value = float(t.value)
-	except ValueError:
-		print ("Line %d: Problem while parsing %s!" % (t.lineno,t.value))
-		t.value = 0
-	return t
+    r'\d+(\.\d+)?'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print ("Line %d: Problem while parsing %s!" % (t.lineno,t.value))
+        t.value = 0
+    return t
 
 def t_COMMENT(t):
-	r'//[A-Za-z _]*'
-	return t
+    r'//[A-Za-z _]*'
+    t.value = t.value[2:]
+    return t
 
 def t_newline(t):
-	r'\n+'
-	t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
 t_ignore  = ' \t'
 
 def t_error(t):
-	print ("Illegal character '%s'" % repr(t.value[0]))
-	t.lexer.skip(1)
+    print ("Illegal character '%s'" % repr(t.value[0]))
+    t.lexer.skip(1)
 
 lex.lex()
 
 if __name__ == "__main__":
-	import sys
-	prog = open(sys.argv[1]).read()
+    import sys
+    prog = open(sys.argv[1]).read()
 
-	lex.input(prog)
+    lex.input(prog)
 
-	while 1:
-		tok = lex.token()
-		if not tok: break
-		print ("line %d: %s(%s)" % (tok.lineno, tok.type, tok.value))
+    while 1:
+        tok = lex.token()
+        if not tok: break
+        print ("line %d: %s(%s)" % (tok.lineno, tok.type, tok.value))
